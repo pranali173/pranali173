14d04b489b9778cdbbbdb74dbe595c9661dbee2e
pranali173 <84122937+pranali173@users.noreply.github.com>
2021-11-08 23:48:48 -0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,39 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Earrings",
+    desc : "Designer Earrings.",
+    img : "img1.jpg",
+    price : 234
+  },
+  124: {
+    name : "necklase",
+    desc : "Golden Necklace.",
+    img : "img2.jpg",
+    price : 247
+  },
+  125: {
+    name : "Mangalsutra",
+    desc : "Diamond Mangalsutra.",
+    img : "img3.jpg",
+    price : 675
+  },
+  126: {
+    name : "Design necklase",
+    desc : "Designer Necklace. ",
+    img : "img4.jpg",
+    price : 842
+  },
+  127: {
+    name : "Earrings",
+    desc : "Golden Earrings.",
+    img : "img5.jpg",
+    price : 937
+  },
+  128: {
+    name : "Earrings",
+    desc : "Diamond Pendant Mangalsutra.",
+    img : "img6.jpg",
+    price : 847
+  },
+};
